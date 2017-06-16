a2e64a3acb3bb0cb4fe50c5335c793da16d3409c
indomenaga <indominaga1@gmail.com>
2017-06-16 17:44:10 +0800

    adding node module and html respond

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-app.js
+var express = require('express');
+var app = express();
+var path = require('path');
+
+
+app.set('port',3000);
+
+app.get('/', function(req, res){
+	console.log("GET the Home page");
+	res
+		.status(200)
+		.sendFile(path.join(__dirname,'public','index.html'));
+});
+
+var server = app.listen(app.get('port'), function(){
+	var port = server.address().port;
+	console.log('Magic happend on port ' + port);
+});
